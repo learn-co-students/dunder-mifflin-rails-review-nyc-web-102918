387d5143340f05b7bee28930e3a2944baef0dd0f
jbmenashi <jbmenashi@gmail.com>
2018-11-28 11:18:07 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,14 +1,16 @@
 class EmployeesController < ApplicationController
+  before_action :find_employee, only: [:show, :edit, :update]
 
   def index
     @employees = Employee.all
   end
 
   def show
-    @employee = Employee.find(params[:id])
+
   end
 
   def new
+    @dogs = Dog.all
     @employee = Employee.new
   end
 
@@ -22,11 +24,10 @@ class EmployeesController < ApplicationController
   end
 
   def edit
-    @employee = Employee.find(params[:id])
+    @dogs = Dog.all
   end
 
   def update
-    @employee = Employee.find(params[:id])
     if @employee.update(employee_params)
       redirect_to employee_path(@employee)
     else
@@ -38,6 +39,10 @@ class EmployeesController < ApplicationController
 
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
+  def find_employee
+    @employee = Employee.find(params[:id])
   end
 
 end
